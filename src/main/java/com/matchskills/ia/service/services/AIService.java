cad68082ed93a610634b02fe3189a767d18cb597
Mayck Gomes <mayck.gomes2901@gmail.com>
2026-08-17 09:48:59 -0300

    chore: removed unused imports

--- src/main/java/com/matchskills/ia/service/services/AIService.java
+++ src/main/java/com/matchskills/ia/service/services/AIService.java
@@ -17,7 +17,6 @@ import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 @Service
 public class AIService {
